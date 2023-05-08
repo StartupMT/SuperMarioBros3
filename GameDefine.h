7197479b2b979fdc3fcd1790116812455d0c0e76
StartupMT <builehung270197@gmail.com>
2023-05-09 00:53:42 +0700

    Add project files.

---
+++ GameDefine.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "dinput.h"
+
+namespace Define
+{
+	//Windows
+	const float GameWidth = 256.0f;
+	const float GameHeight = 240.0f;
+	const float WinWidth = 768.0f;
+	const float WinHeight = 720.0f;
+	const float FPS = 60.0f;
+}
