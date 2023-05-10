589442e31a5a47d2950c8dc70468d3806c7432de
StartupMT <builehung270197@gmail.com>
2023-05-11 01:51:14 +0700

    Tạo bối cảnh

---
+++ SceneManager.cpp
@@ -0,0 +1,35 @@
+﻿#include "SceneManager.h"
+
+SceneManager* SceneManager::_sceneManager;
+SceneManager* SceneManager::GetInstance()
+{
+	if (nullptr == _sceneManager) {
+		_sceneManager = new SceneManager();
+	}
+	return _sceneManager;
+}
+
+SceneManager::SceneManager()
+{
+}
+SceneManager::~SceneManager()
+{
+}
+
+//Load Data Game
+void SceneManager::InitDT(Graphic* graphic)
+{
+	ObjectManager::GetInstance()->InitDT(graphic);
+}
+
+//Update các scene game Update lớn nhất
+void SceneManager::Update(float gameTime, Keyboard* key)
+{
+	ObjectManager::GetInstance()->Update(gameTime, key);
+}
+
+//Vẽ Object lên màn hình
+void SceneManager::Render()
+{
+	ObjectManager::GetInstance()->Render();
+}
---
+++ SceneManager.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Graphic.h"
+#include "Keyboard.h"
+#include "ObjectManager.h"
+#include "GameDefine.h"
+
+using namespace Define;
+
+class SceneManager
+{
+	static SceneManager* _sceneManager;
+public:
+	SceneManager();
+	~SceneManager();
+
+	//Load Data Game
+	void InitDT(Graphic* graphic);
+
+	//Update Game sau khoảng thời gian gameTime
+	void Update(float gameTime, Keyboard* key);
+	//Vẽ Object lên màn hình
+	void Render();
+	static SceneManager* GetInstance();
+};
+
